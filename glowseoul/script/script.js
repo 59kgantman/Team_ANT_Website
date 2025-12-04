c7493ebebf7e3c5ebbf861bd531daa57770137f9
59kgantman <boham1025@gmail.com>
2025-12-04 19:34:27 +0900

    햄버거 버튼 생성 및 햄버거 메뉴 애니메이션 삽입, 메인 비디오 교체, 세부 디테일 작업

--- glowseoul/script/script.js
+++ glowseoul/script/script.js
@@ -1,1 +1,13 @@
 console.log("Hello");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const mobileNav = document.querySelector(".mobile-nav");
+
+  if (!hamburger || !mobileNav) return;
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileNav.classList.toggle("open");
+  });
+});
